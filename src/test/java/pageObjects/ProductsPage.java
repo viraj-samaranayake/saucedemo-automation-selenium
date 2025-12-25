9171ff6f6aa702bcac7ef0eaec2889128c4c8d55
codevrj <isuruviraj25@gmail.com>
2025-12-25 17:35:56 +0530

    Products page-object updated

--- src/test/java/pageObjects/ProductsPage.java
+++ src/test/java/pageObjects/ProductsPage.java
@@ -12,8 +12,30 @@ public class ProductsPage {
     }
 
     private final By pageTitle = By.cssSelector("span.title");
+    private final By hamBurgerIcon = By.cssSelector("button#react-burger-menu-btn");
+    private final By logoutIcon = By.cssSelector("a#logout_sidebar_link");
+
 
     public String getPageTitle() {
         return driver.findElement(pageTitle).getText();
     }
+
+    public boolean verifyPageTitle(){
+        String title = "Products";
+        return title.equals(getPageTitle());
+    }
+
+    public boolean verifyUrl(){
+        return driver.getCurrentUrl().contains("inventory");
+    }
+
+    public void clickHamburgerIcon(){
+        driver.findElement(hamBurgerIcon).click();
+    }
+
+    public void clickLogout(){
+        driver.findElement(logoutIcon).click();
+    }
+
+
 }
