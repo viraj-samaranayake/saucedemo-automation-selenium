fafaff5292eb9ce1a8e9defeb7691d51f4a179e4
codevrj <isuruviraj25@gmail.com>
2025-12-25 15:53:01 +0530

    BaseClass updated

--- src/test/java/testCases/BaseClass.java
+++ src/test/java/testCases/BaseClass.java
@@ -4,18 +4,31 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 public class BaseClass {
 
     public static WebDriver driver = new ChromeDriver();
 
-    @BeforeMethod
-    public void setup(){
+    public Properties properties;
+
+    @BeforeClass
+    public void setup() throws IOException {
+
+        FileReader fileReader = new FileReader("src/test/resources/config.properties");
+        properties = new Properties();
+        properties.load(fileReader);
+
+        String url = properties.getProperty("baseUrl");  // Reading url from properties file
+
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
-        driver.get("https://www.saucedemo.com/");
+        driver.get(url);
     }
 
-    @AfterMethod
+    @AfterClass
     public void tearDown(){
         driver.quit();
     }
